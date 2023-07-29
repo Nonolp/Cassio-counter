e4e76318e518d59d23358bbd14eed31169a1ce30
Nonolp <78102501+Nonolp@users.noreply.github.com>
2023-07-29 21:33:10 +0200

    main.cpp
    Complete program of v1.0

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+
+
+int main (){
+    
+    //type name;
+    //char - character -'r'
+    // int - integer - 50,42,500, -400
+    //float - floating points -4.2f
+    //double - double precision -5.0
+   int a = 50;
+   int b = 40;
+   int c = a + b;
+
+    std ::cout <<"Hello, World"<<std::endl;
+    std::cin.ignore();
+    return 0;
+}
